a2f9f1eba1521a3362bcd4df414e930efa31974a
Justnhan <nhandz1252006@gmail.com>
2026-06-24 17:05:00 +0700

    xong

--- Giao diện/main.py
+++ Giao diện/main.py
@@ -2,7 +2,7 @@ import sys
 import os
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.uic import loadUiType
-from PyQt6.QtGui import QFont
+
 
 # Load file .ui thành Class giao diện trước khi chạy code
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -13,10 +13,6 @@ class MyWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self) # Khởi tạo giao diện từ file .ui vừa load
-
-
-
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
